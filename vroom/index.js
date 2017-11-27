c4f4e5c9d4370d52b88ecd20684d6ccce79056f1
Elton Rego <rego.elton@gmail.com>
2017-11-27 15:10:33 -0800

    Added some general cleanups to these files

--- vroom/index.js
+++ vroom/index.js
@@ -3,20 +3,9 @@
  * Please delete components that aren't used.
  */
 
-// Global Requirements
-import React from 'react';
-GLOBAL = require('./src/Globals');
-
 // Components
+import { AppRegistry } from 'react-native';
 import Auth from "./src/components/Login/Auth";
-import { 
-  SignedOut,
-  SignedIn,
-} from "./src/components/Navigation/Router";
-import {
-  AppRegistry,
-} from 'react-native';
-import {firebaseRef} from './src/components/Database/Database';
 
-// Pushes the Navigation Stack onto the View
+// Pushes the Authentication Class onto the View
 AppRegistry.registerComponent('vroom', () => Auth);
