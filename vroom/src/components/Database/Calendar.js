41bd56890eb1237f44ab70a9d77832da100771f1
Connick Shields <connickshields@gmail.com>
2017-11-30 22:07:27 -0800

    changing Events to Tasks

--- vroom/src/components/Database/Calendar.js
+++ vroom/src/components/Database/Calendar.js
@@ -1,18 +1,18 @@
 import {firebaseRef} from "./Database";
 
 /*
-* Database function: getEventDates()
+* Database function: getTaskDates()
 * Author: Connick Shields
 *
-* Purpose: get a string list of event dates to use with the calendar
+* Purpose: get a string list of task dates to use with the calendar
 *
 * @param: (u) = user
-* @return: a list of event dates to be used when creating the calendar
+* @return: a list of task dates to be used when creating the calendar
 */
 
-export function getEventDates(u){
+export function getTaskDates(u){
     return new Promise((resolve, reject) => {
-      firebaseRef.firestore().collection("users").doc(u).collection("events")
+      firebaseRef.firestore().collection("users").doc(u).collection("tasks")
       .get()
       .then(function(querySnapshot) {
           var dates = {};
@@ -24,22 +24,35 @@ export function getEventDates(u){
           resolve(dates);
       })
       .catch(function(error) {
-          console.log("Error getting events: ", error);
+          console.log("Error getting tasks: ", error);
           reject(error);
       });
     });
 }
 
 /*
-* Database function: getEventByDate()
+* Database function: getTaskByDate()
 * Author: Connick Shields
 *
-* Purpose: get a list of Calendar events by date
+* Purpose: get a list of Calendar tasks by date
 *
 * @param: (d) = date
-* @return: a list of event(s) that will occur on the specified date
+* @param: (u) = user
+* @return: an array of task(s) that will occur on the specified date
 */
 
-export function getEventByDate(d){
+export function getTaskByDate(d, u){
+  return new Promise((resolve, reject) => {
+    firebaseRef.firestore().collection("users").doc(u).collection("tasks")
+    .get().where(doc.id, "==", d)
+    .then(function(data) {
 
+        console.log(data);
+        resolve(data);
+    })
+    .catch(function(error) {
+        console.log("Error getting tasks: ", error);
+        reject(error);
+    });
+  });
 }
