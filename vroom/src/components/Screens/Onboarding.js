2c11a12ba819a23f21d093ef5443916a029f132c
Alec Felt <allfelt@ucsc.edu>
2017-12-03 23:18:21 -0800

    fixed Will and Payam's code

--- vroom/src/components/Screens/Onboarding.js
+++ vroom/src/components/Screens/Onboarding.js
@@ -97,6 +97,7 @@ export default class Onboarding extends Component {
       model: null,
       year: null,
       choices: null,
+      array: null,
     };
   }
 
@@ -115,7 +116,7 @@ export default class Onboarding extends Component {
       this.scrollView.scrollTo({x: this.state.scroll_pos + 344, y:0, animated: true});
       this.setState({
         scroll_enabled: false,
-        text_button: `Continue to ${this.state.text}`
+        text_button: `Continue to ${this.state.text}`,
       });
       this.animation3.play();
       // populates user's Firebase entry
@@ -123,7 +124,6 @@ export default class Onboarding extends Component {
           nickname: this.state.text,
           path: "cars/" + this.state.year + "/" + this.state.make + "/" + this.state.model,
           choices: this.state.choices,
-
       });
     // make,year,model dropdowns haven't been properly set
     } else {
@@ -174,14 +174,14 @@ export default class Onboarding extends Component {
   }
 
 arrayToJSON(array){
-    console.log("input array");
+    console.log("input array: ");
     console.log(array);
-    console.log("array type is", typeof array); 
+    console.log("array type is", typeof array);
     console.log("array length");
     console.log(array.length);
     console.log("array[0] =", array[0]);
     console.log("initial JSON object");
-    var retObj = [{value: array[0]}];
+    var retObj = [{value: array["0"]}];
     console.log(retObj);
     var newObj;
     for (i = 1; i < array.length; i++){
@@ -196,19 +196,16 @@ arrayToJSON(array){
 
   getModelYears(){
     var ret = [];
-
+    var that = this;
     firebaseRef.database().ref('cars').once("value")
     .then(function(snapshot){
       snapshot.forEach(function(childSnapshot){
+        console.log("yo");
         ret[ret.length] = childSnapshot.key;
       });
+      that.arrayToJSON(ret);
     });
-    console.log("array");
-    console.log(ret);
-    var rett = this.arrayToJSON(ret);
-    console.log("JSON");
-    console.log(rett);
-    return rett;
+    return ret;
   }
 
 
