f692235bd70c7fd42f18ae2985db94f08acb6acd
Elton Rego <rego.elton@gmail.com>
2017-12-08 13:48:39 -0800

    Fixed stupid yellow warning

--- vroom/src/components/Login/Login.js
+++ vroom/src/components/Login/Login.js
@@ -36,6 +36,7 @@ export default class Login extends Component {
       input_style1: STYLE.input_inactive,
       input_style2: STYLE.input_inactive,
       input_style3: STYLE.input_inactive,
+      button_color: GLOBAL.COLOR.GREEN,
 
       sign_up: false,
       email: null,
@@ -137,7 +138,16 @@ export default class Login extends Component {
   * Purpose: When called, shakes the button
   */
   shakeButton(){
+    console.disableYellowBox = true;
     Animated.sequence([
+      Animated.timing(this.state.shake_animation, {
+        toValue: -8,                   
+        duration: 50,              
+      }),
+      Animated.timing(this.state.shake_animation, {
+        toValue: 8,                   
+        duration: 50,              
+      }),
       Animated.timing(this.state.shake_animation, {
         toValue: -8,                   
         duration: 50,              
@@ -312,7 +322,10 @@ export default class Login extends Component {
             />
             {pw_confirm_field}
             <Animated.View style={{transform: [{translateX: this.state.shake_animation}]}}>
-              <TouchableOpacity style={STYLE.button_container}
+              <TouchableOpacity style={[STYLE.button_container, 
+                {
+                  backgroundColor: this.state.button_color,
+                }]}
                 onPress={()=>{
                   if(this.state.sign_up){
                     this.signup();
--- vroom/src/global-styles.js
+++ vroom/src/global-styles.js
@@ -48,7 +48,7 @@ module.exports = StyleSheet.create({
   * Purpose: Styles a global green submit button
   */
   button_container:{
-    backgroundColor: GLOBAL.COLOR.GREEN,
+    // backgroundColor: GLOBAL.COLOR.GREEN,
     borderRadius: 4,
     paddingHorizontal: 16,
     paddingVertical: 12,
