8225c541c5fb299b3d6337b207af11fd0882620e
Elton Rego <rego.elton@gmail.com>
2017-12-08 22:27:58 -0800

    More work done on onboarding

--- vroom/src/components/Screens/r2Onboarding.js
+++ vroom/src/components/Screens/r2Onboarding.js
@@ -8,7 +8,59 @@ import {
   View,
   StatusBar,
   StyleSheet,
+  Text,
+  TouchableOpacity,
 } from 'react-native';
+import Animation from 'lottie-react-native';
+
+// Files Needed
+import revi from '../../../assets/animations/revi-hi.json';
+import revi_on from '../../../assets/animations/revi-on.json';
+import revi_super_happy from '../../../assets/animations/revi-super-happy.json';
+
+class Screens extends Component {
+
+  /*
+   * Method: componentDidMount()
+   * Author: Elton C. Rego
+   *
+   * Purpose: When a component specified sucessfully is rendered,
+   *   it runs the action
+   */
+  componentDidMount() {
+    console.log("Onboarding screens component mounted");
+    this.animation.play();
+  }
+
+  render() {
+    return (
+      <View style={[STYLE.container, styles.onboarding_container]}>
+        <StatusBar barStyle="light-content"/>
+        <View style={[STYLE.card_focused, styles.onboarding_card]}>
+          <Text style={STYLE.light_title2_center}>Hello!</Text>
+          <View style={styles.onboarding_animations}>
+            <Animation
+              ref={animation => {this.animation = animation;}}
+              style={{width: '100%', height: '100%',}}
+              loop={false}
+              source={revi}
+            />
+          </View>
+          <Text style={STYLE.light_subheader_center}>I'm your car</Text>
+        </View>
+        <TouchableOpacity style={[STYLE.button_container, 
+          {
+            backgroundColor: GLOBAL.COLOR.GREEN,
+            width: 224,
+            marginTop: 32,
+          }
+        ]}>
+          <Text style={STYLE.green_button_text}>next</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 /*
  * Class: Onboarding
@@ -36,12 +88,7 @@ export default class Onboarding extends Component{
   */
   render() {
     return (
-      <View style={[STYLE.container, styles.onboarding_container]}>
-        <StatusBar barStyle="light-content"/>
-        <View 
-          style={STYLE.card_focused}>
-        </View>
-      </View>
+      <Screens/>
     );
   }
 
@@ -53,14 +100,33 @@ export default class Onboarding extends Component{
  */
 const styles = StyleSheet.create({
 
-  /*
-   * Style: Button
-   * Author: Elton C. Rego
-   * Purpose: This styles the onboarding_container on top of default
-   */
+ /*
+  * Style: Button
+  * Author: Elton C. Rego
+  * Purpose: This styles the onboarding_container on top of default
+  */
   onboarding_container: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  onboarding_card: {
+    height: 440,
+    width: 288,
+    padding: 32,
+    justifyContent: 'space-between',
+  },
+
+ /*
+  * Style: Onboarding Animations
+  * Author: Elton C. Rego
+  * Purpose: This styles the Revis on each card
+  */
+  onboarding_animations: {
+    alignSelf: 'center',
+    height: 256,
+    width: 256,
+    margin: -32,
   },
 
 });
