98b5cf556cb4fa6d2999c8ffe71fe7cff608d8ed
Alec Felt <allfelt@ucsc.edu>
2017-12-11 09:55:44 -0800

    refactored Connick's getTaskByDate() in Calendar.js using the extraction method by providing a helper function called loopThruTask. I have clinical ocd, this wasn't actually necessary lol

--- vroom/src/components/Database/Calendar.js
+++ vroom/src/components/Database/Calendar.js
@@ -28,8 +28,51 @@ export function getTaskDates(u){
 }
 
 /*
+* Database function: loopThruTasks()
+* Author: Connick Shields, Alec Felt
+*
+* Purpose: loops through the children of a Firebase DataSnapshot,
+*     helper function for getTaskByDate(),
+*     returns a promise that resolves with an array of tasks
+*
+* @param: (d) = date
+* @param: (snap) = Firbase DataSnapshot
+* @return: an array of task(s) that will occur on the specified date
+*/
+function loopThruTasks(d, snap){
+    return new Promise((resolve, reject) => {
+        // keep track of the number of tasks we process so we can return the data at the right time
+        var data = [], numProcessed = 0;
+        snap.forEach(function(child) {
+            if(child.val().date == d){
+                //child.val() is the task
+                //set ttref of that to the path to the specific task type
+                //do snap of the task type at the end of that path
+                // in Dashboard.js in onDayPress, display that info
+                firebaseRef.database().ref(child.val().ttRef).once('value').then(function(tt){
+                    data[data.length] = {key:child.key,title:tt.val().action+" "+tt.val().item, desc:tt.val().itemDescription};
+                    numProcessed++;
+                    if(numProcessed == snap.numChildren()){
+                        resolve(data);
+                    }
+                })
+                .catch(function(error){
+                    console.log("ERROR loopThruTasks: "+error.message);
+                    reject(error);
+                });
+            } else {
+                numProcessed++;
+                if(numProcessed == snap.numChildren()){
+                    resolve(data);
+                }
+            }
+        });
+    });
+}
+
+/*
 * Database function: getTaskByDate()
-* Author: Connick Shields
+* Author: Connick Shields, Alec Felt
 *
 * Purpose: get a list of Calendar tasks by date
 *
@@ -37,38 +80,20 @@ export function getTaskDates(u){
 * @param: (u) = user
 * @return: an array of task(s) that will occur on the specified date
 */
-
 export function getTaskByDate(d, u){
     return new Promise((resolve, reject) => {
         firebaseRef.database().ref('tasks').orderByChild('uid').equalTo(u).once('value')
         .then(function(snapshot) {
+            console.log("getTaskByDate(): task query success");
             var data = [];
             // make sure the snapshot has data in it
             if(snapshot.exists()){
-                // keep track of the number of tasks we process so we can return the data at the right time
-                var numProcessed = 0;
-                snapshot.forEach(function(child) {
-                    console.log("numChildren:"+snapshot.numChildren());
-                    if(child.val().date == d){
-                        //child.val() is the task
-                        //set ttref of that to the path to the specific task type
-                        //do snapshot of the task type at the end of that path
-                        // in Dashboard.js in onDayPress, display that info
-                        firebaseRef.database().ref(child.val().ttRef).once('value').then(function(tt){
-                            data[data.length] = {key:child.key,title:tt.val().action+" "+tt.val().item, desc:tt.val().itemDescription};
-                            numProcessed++;
-                            if(numProcessed == snapshot.numChildren()){
-                                console.log("exiting match: "+child.key+" "+numProcessed);
-                                resolve(data);
-                            }
-                        });
-                    } else {
-                        numProcessed++;
-                        if(numProcessed == snapshot.numChildren()){
-                          console.log("exiting no match: "+child.key+" "+numProcessed);
-                          resolve(data);
-                        }
-                    }
+                console.log("getTaskByDate(): task query exists");
+                loopThruTasks(d, snapshot).then(arr => {
+                    console.log("resolved array: "+arr);
+                    resolve(arr);
+                }).catch((error) => {
+                    console.log(error.message);
                 });
             } else {
                 resolve(data);
