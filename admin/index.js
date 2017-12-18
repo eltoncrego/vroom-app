d044655c8f0c3f039067757e2bc8ae6fcaa36845
Alec Felt <allfelt@ucsc.edu>
2017-12-18 01:10:01 -0800

    task creation script is now live! check out the enter-task script in vroom-app/admin/package.json

--- admin/index.js
+++ admin/index.js
@@ -1,43 +1,15 @@
-const admin = require('firebase-admin');
+const adminFirebase = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey');
-const prompt = require('prompt');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+/*
+ * Firebase Admin SDK Configuration
+ * Author: Alec Felt
+ *
+ * Purpose: Allows Firebase Admin functionality
+ */
+var config = {
+  credential: adminFirebase.credential.cert(serviceAccount),
   databaseURL: "https://vroom-d5c0e.firebaseio.com"
-});
-
-console.log("=======================================");
-console.log("Which car?");
-console.log("=======================================");
-
-prompt.start();
-
-var carProps = {
-    properties: {
-        year: {
-            description: 'Year',
-            pattern: /\d{4}/,
-            required: true
-        },
-        make: {
-            description: 'Make',
-            pattern: /^[A-Z][a-z]*/,
-            required: true
-        },
-        model: {
-            description: 'Model',
-            required: true
-        }
-    }
-}
-
-prompt.get(carProps, function (err, result) {
-    admin.database().ref().child(result.year).child(result.make).child(result.model).on('value', function(snap) {
-        if(snap.exists()){
-            console.log("car exists!");
-        }else{
-            console.log("car doesn't exist!");
-        }
-    });
-});
+};
+const admin = adminFirebase.initializeApp(config);
+module.exports = admin;
