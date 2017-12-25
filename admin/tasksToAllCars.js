7fc03f6423fc4303693bcc7a98646395c59d12de
Alec Felt <allfelt@ucsc.edu>
2017-12-24 23:42:51 -0800

    cleaned up Will's script a bit and added the start of my assigned car json files

--- admin/tasksToAllCars.js
+++ admin/tasksToAllCars.js
@@ -1,19 +1,19 @@
-/* 
+/*
  * tasksToAllCars.js
- * Author: Will Coates 
+ * Author: Will Coates
  * 		Acknowledgments:
  *			-Alec Felt's work on taskCreation.js was a big help
  			-stackOverflow, the font of all knowledge
- * 
- * A script to update each car model in the database with a set of tasks, 
+ *
+ * A script to update each car model in the database with a set of tasks,
  * 	represented by a given JSON object.
- * 
+ *
  * Invoke from the command line by running "node tasksToAllCars.js"
  * 		(ideally I'd have a script/alias set up, but I can't even run Alec's script)
- * 
+ *
  */
 
- /* 
+ /*
   * Firebase Configuration
   * Author: Alec Felt
   *
@@ -34,14 +34,14 @@ const admin = require('./index');
  * Globals: jsonPrompt
  * Author: Will Coates
  *
- * Purpose: sets up prompting user for JSON object to be written 
+ * Purpose: sets up prompting user for JSON object to be written
  * 			(path should be something like: '../')
  */
 var jsonPrompt = {
 	properties: {
 		filename: {
 			description: "Please enter the filename of the JSON object",
-			// regular expression should match any .json filename 
+			// regular expression should match any .json filename
 			pattern: /^[\w, \s-]+\.json$/,
 			message: 'Must be a .json file',
 			required: true
@@ -57,53 +57,49 @@ console.log("This script updates each car model in the database with a set of ta
 console.log("The JSON object should be a .json file in admin/taskJSONObjects");
 console.log("=====================================================================================================");
 
-
-
 /*
  * writeToAllCars()
  * Author: Will Coates
- * 
- * Purpose: 
+ *
+ * Purpose:
  		writes a given JSON object to every car in the database
  * Variables:
  		tasks: JSON object to write to each car
  */
-
 writeToAllCars = (tasks) => {
-   var query = admin.database().ref('cars').orderByKey();
-   query.once('value')
-   	.then(function(carSnapshot) {
-   		// take a snapshot of all the years
-   		carSnapshot.forEach(function(yearSnapshot) {
-   			var year = yearSnapshot.key;
-        	// for each year, take a snapshot of all the makes
-   			yearSnapshot.forEach(function(makeSnapshot) {
-   				var make = makeSnapshot.key;
-   				// for each make, take a snapshot of all the models
-   				makeSnapshot.forEach(function(modelSnapshot) {
-   					var model = modelSnapshot.key;
-				    // for each model, update task_types with object: tasks 
-   					admin.database().ref('cars').child(year).child(make).child(model)
-   					.child('task_types').update(tasks)
-   					.catch((err) =>{
-   						console.log(err.message);
-   					});
-   					console.log('Written to ' + year + ' ' + make + ' ' + model);
-   				});
-   			});
-			console.log('Written to all cars of year: ' + year);
-			
-			// must release the database so Node.js knows there are no more events that can happen
-  			console.log("Database is going offline");
-			admin.database().goOffline();
-   		});
-   	}); 
+     var query = admin.database().ref('cars').orderByKey();
+     query.once('value')
+     .then(function(carSnapshot) {
+     		// take a snapshot of all the years
+     		carSnapshot.forEach(function(yearSnapshot) {
+     			var year = yearSnapshot.key;
+          // for each year, take a snapshot of all the makes
+     			yearSnapshot.forEach(function(makeSnapshot) {
+     				var make = makeSnapshot.key;
+     				// for each make, take a snapshot of all the models
+     				makeSnapshot.forEach(function(modelSnapshot) {
+     					var model = modelSnapshot.key;
+  				    // for each model, update task_types with object: tasks
+     					admin.database().ref('cars').child(year).child(make).child(model)
+     					.child('task_types').update(tasks)
+     					.catch((err) => {
+     						console.log(err.message);
+     					});
+              console.log('Written to ' + year + ' ' + make + ' ' + model);
+     				});
+     			});
+  			  console.log('Written to all cars of year: ' + year);
+     		});
+        // must release the database so Node.js knows there are no more events that can happen
+        console.log("Database is going offline");
+    		admin.database().goOffline();
+     });
 }
 
-/* 
+/*
  * Collect and Write Tasks
  * Author: Will Coates
- * 
+ *
  * Purpose: collects the JSON object that the admin wants to write, and
  *			actually writes it to each car in the database
  */
@@ -116,26 +112,23 @@ prompt.get(jsonPrompt, function(err, result){
    // in taskJSONObjects, then set to tasksOb, otherwise catch
    // "Error: Cannot find module..."
    try{
-   	var tasksOb = require('../admin/taskJSONObjects/' + filename );
+   	 var tasksOb = require('../admin/taskJSONObjects/' + filename );
    }
    catch(ex){
-    console.error('Error: ', ex.message);
-    console.log('Please try again');
-    return; 
+     console.error('Error: ', ex.message);
+     console.log('Please try again');
+     return;
    }
 
-   console.log('tasksOb: ' + tasksOb);
+   console.log('tasksOb: ' + JSON.stringify(tasksOb));
 
    // Now update tasksOb to all existing cars in the database
    var promise = new Promise(function(resolve) {
    		writeToAllCars(tasksOb);
-   		resolve("yay");
+   		resolve("execution has left tasksToAllCars script.");
    });
 
    promise.then(function(result) {
    	console.log(result);
    });
 });
-
-
-
