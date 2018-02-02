8b1cff090c3172ae8659119d3df977b38ea454a0
Ryan <rccho@ucsc.edu>
2018-02-01 19:18:24 -0800

    removed svg files and imports

--- vroom/android/app/src/main/java/com/vroom/MainApplication.java
+++ vroom/android/app/src/main/java/com/vroom/MainApplication.java
@@ -3,7 +3,6 @@ package com.vroom;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; 
@@ -29,12 +28,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
-            new LottiePackage(),
+          new LottiePackage(),
           new RNFirebasePackage(),
 		  new RNFirebaseDatabasePackage(),
 		  new RNFirebaseAuthPackage(),
-            new LottiePackage()
+          new LottiePackage()
       );
     }
 
