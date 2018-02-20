0fdfc5b7e7f88ead0cb3f15df05cdbb900313a4b
Elton C. Rego <rego.elton@gmail.com>
2018-02-19 22:32:03 -0800

    one more minor bug fix

--- vroom/src/components/Screens/Dashboard.js
+++ vroom/src/components/Screens/Dashboard.js
@@ -725,7 +725,7 @@ const styles = StyleSheet.create({
   },
   card: {
     width: '100%',
-    minHeight: '90%',
+    maxHeight: '90%',
     backgroundColor: GLOBAL.COLOR.WHITE,
     zIndex: 1,
   },
