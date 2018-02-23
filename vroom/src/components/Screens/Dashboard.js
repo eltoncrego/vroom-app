3a4825c6c4a2b96d3542a31bf3d7ac4ca6f7abf4
Elton C. Rego <rego.elton@gmail.com>
2018-02-22 18:01:01 -0800

    Revert "Switched to using date objects"
    This reverts commit 4d33cd97fff13ca2884022eff716fbc38f694033.

--- vroom/src/components/Screens/Dashboard.js
+++ vroom/src/components/Screens/Dashboard.js
@@ -145,6 +145,11 @@ export default class Dashboard extends Component {
     this.setState({modalVisible:false});
   }
 
+  getFormattedTime() {
+    var todaysDate = moment().format('MMM DD, YYYY - h:mma');
+    return todaysDate;
+  }
+
  /*
   * Function: addItem()
   * Author: Elton C. Rego
@@ -198,7 +203,7 @@ export default class Dashboard extends Component {
     const mpg = distance/this.state.user_filled;
     const average =
       ((this.state.averageMPG * (this.state.textDataArr.length))+mpg)/(this.state.textDataArr.length+1);
-    const creationDate = moment();
+    const creationDate = this.getFormattedTime();
 
     this.closeModal();
 
@@ -215,7 +220,17 @@ export default class Dashboard extends Component {
     this.setState({
       averageMPG: average,
       updatedODO: this.state.user_ODO,
-      textDataArr: [newFillup, ...this.state.textDataArr],
+      textDataArr:
+      [
+        {
+          list_i: this.state.list_i + 1,
+          totalPrice: parseFloat(this.state.user_paid),
+          date: creationDate,
+          gallonsFilled: this.state.user_filled,
+          odometer: this.state.user_ODO,
+          distanceSinceLast: distance
+        }, ...this.state.textDataArr
+      ],
       user_paid: 0,
       user_filled: 0,
       user_ODO: 0,
