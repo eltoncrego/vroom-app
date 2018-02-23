3d0903602e0c7564b2642c90183078dc47f3c01a
Elton C. Rego <rego.elton@gmail.com>
2018-02-22 16:45:11 -0800

    Did more cleaning of FB Trash

--- vroom/android/app/src/main/java/com/vroom/MainActivity.java
+++ vroom/android/app/src/main/java/com/vroom/MainActivity.java
@@ -4,17 +4,13 @@ import com.facebook.react.ReactActivity;
 
 import android.content.Intent;
 
-import com.facebook.CallbackManager;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ReactActivity {
-
-    CallbackManager mCallbackManager;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -26,20 +22,10 @@ public class MainActivity extends ReactActivity {
     }
 
     protected List<ReactPackage> getPackages() {
-        mCallbackManager = new CallbackManager.Factory().create();
         ReactPackage packages[] = new ReactPackage[]{
                 new MainReactPackage(),
-                new FBSDKPackage(mCallbackManager),
         };
 
         return Arrays.<ReactPackage>asList(packages);
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
-    }
-
 }
--- vroom/android/app/src/main/java/com/vroom/MainApplication.java
+++ vroom/android/app/src/main/java/com/vroom/MainApplication.java
@@ -2,28 +2,17 @@ package com.vroom;
 
 import android.app.Application;
 
-import com.facebook.CallbackManager;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import io.callstack.react.fbads.FBAdsPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
@@ -34,15 +23,9 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      mCallbackManager = new CallbackManager.Factory().create();
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new FBSDKPackage(),
-            new FBSDKPackage(),
-            new FBAdsPackage(),
-            new LottiePackage(),
-        new FBSDKPackage(mCallbackManager),
-        new FBAdsPackage(),
+        new LottiePackage(),
         new LottiePackage()
       );
     }
@@ -58,10 +41,4 @@ public class MainApplication extends Application implements ReactApplication {
     return mReactNativeHost;
   }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    AppEventsLogger.activateApp(this);
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
