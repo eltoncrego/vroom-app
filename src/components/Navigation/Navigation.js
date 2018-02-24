0ad8476b94f3060c30171ee4f18bba58edce0384
Elton C. Rego <rego.elton@gmail.com>
2018-02-24 12:49:58 -0800

    ReCommented Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,34 +2,6 @@ import React from 'react';
 import { NavigationActions } from 'react-navigation'
 
 /*
-* Navigation function: clearNavStack
-* Author: Alec Felt
-*
-* Purpose: clears current navigation Stack
-           navigates to specified component (page)
-*
-* @param: (nav) = navigation prop for component
-*         (page) = string specifiying name of component to go to
-* @return: void
-*
-* https://reactnavigation.org/docs/navigators/navigation-actions
-*/
-export function clearNavStack(nav, page) {
-  console.log("clearNavStack called");
-  const resetAction = NavigationActions.reset({
-    index: 0,
-    actions: [
-      NavigationActions.navigate({ routeName: page})
-    ]
-  });
-  nav.dispatch(resetAction);
-}
-
-export function goBack(nav) {
-  nav.dispatch(NavigationActions.back())
-}
-
-/*
 * Navigation function: goTo
 * Author: Alec Felt
 *
@@ -43,4 +15,18 @@ export function goTo(nav, page) {
   console.log("goTo called")
   const { navigate } = nav;
   navigate(page);
+}
+
+/*
+* Navigation function: goBack
+* Author: Elton C. Rego
+*
+* Purpose: navigate to the previous page in the stack. Please use this
+*   so that we dont have to reset the navigation stack all the time.
+*
+* @param: (nav) = navigation prop for component
+* @return: void
+*/
+export function goBack(nav){
+  nav.goBack();
 }
