a09fb95bd2778f21ddd9667f751f6e392fd79082
Elton C. Rego <rego.elton@gmail.com>
2018-03-01 17:26:18 -0800

    Added react native fcm

--- android/app/src/main/java/tech/revi/vroom/MainApplication.java
+++ android/app/src/main/java/tech/revi/vroom/MainApplication.java
@@ -3,6 +3,7 @@ package tech.revi.vroom;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new FIRMessagingPackage(),
         new LottiePackage()
       );
     }
