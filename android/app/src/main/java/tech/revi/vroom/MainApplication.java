3a72b6d45789284f9a5677e28c8f2185f99d646e
Connick Shields <connickshields@gmail.com>
2018-03-05 13:07:26 -0800

    messing around with the AreaChart

--- android/app/src/main/java/tech/revi/vroom/MainApplication.java
+++ android/app/src/main/java/tech/revi/vroom/MainApplication.java
@@ -3,6 +3,7 @@ package tech.revi.vroom;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new SvgPackage(),
         new FIRMessagingPackage(),
         new LottiePackage()
       );
