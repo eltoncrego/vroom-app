07d878b9a3e3adcab3e14a4dd8e036906c2f17e1
Elton C. Rego <rego.elton@gmail.com>
2018-03-07 18:53:07 -0800

    Globals.js cleaning

--- src/Globals.js
+++ src/Globals.js
@@ -1,26 +1,29 @@
-// Necessary Import
+// Necessary Imports
 'use strict';
 var React = require('react-native');
-var {
-  StyleSheet,
-} = React;
-
-import {
-  Platform,
-} from 'react-native';
 
 // Create Global Colors
 module.exports = {
   COLOR: {
+    // Used for backgrounds and light text color
     WHITE: '#FFFFFF',
+    // Used for promotional parts or texture background
     LIGHTGRAY: "#F2F5F5",
+    // Used for a null button or object
     GRAY: '#92989B',
+    // Used as a background or dark text color
     DARKGRAY: '#253237',
+    // Used to signify an error
     RED: '#DD6372',
+    // Used to signify a success
     GREEN: '#B8E986',
+    // Used to signify a warning
     YELLOW: '#F8E778',
+    // Used to brand periods and main branding elements
     BRAND: '#9FD356',
+    // Used for the settings panel or secondary panel backgrounds
     DARKBLUE: '#4F6E7C',
+    // Used for highlighting on a secondary panel
     BLUE: '#C2DFE3',
   },
 };
