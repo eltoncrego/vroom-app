340654af85b1c338a74b37deb41c82b2e66b173c
Elton C. Rego <rego.elton@gmail.com>
2018-03-11 23:55:52 -0700

    Fixed x-axis

--- src/components/Screens/Dashboard.js
+++ src/components/Screens/Dashboard.js
@@ -658,8 +658,7 @@ export default class Dashboard extends Component {
     var transformTransaction = {transform: [{translateY: transactionTranslation}]};
 
     // adding contentInset prop to ensure consistency between chart and yAxis
-    //var contentInset = { top: 32, bottom: 50, right: -2, left: -2};
-    var contentInset = { top: 32, bottom: 50, right: -2, left: -2};
+    var contentInset = { top: 32, bottom: 100, right: -2, left: -2};
 
     return(
 
@@ -798,7 +797,7 @@ export default class Dashboard extends Component {
               curve={shape.curveNatural}
               contentInset={ contentInset }
               numberOfTicks={3}
-              showGrid={true}
+              showGrid={false}
               svg={{
                 stroke: GLOBAL.COLOR.GREEN,
                 strokeWidth: 3,
@@ -806,16 +805,15 @@ export default class Dashboard extends Component {
               }}
             />
             <XAxis
-                    style={[styles.areaGraph, {zIndex: 1, position: "absolute", }]}
-                    data={this.state.textDataArr}
-                    xAccessor={({item}) => item.list_i}
-                    formatLabel={ value => `Fillup ${value}` }
-                    contentInset={ contentInset }
-                    svg={{
-                            fill: GLOBAL.COLOR.WHITE,
-                            fontSize: 15,
-                        }}
-                />
+              style={[styles.areaGraph, {zIndex: 1,}]}
+              data={this.state.textDataArr}
+              xAccessor={({item}) => item.list_i}
+              formatLabel={value => `${value}`}
+              svg={{
+                fill: GLOBAL.COLOR.WHITE,
+                fontSize: 15,
+              }}
+            />
         </Animated.View>
             <Animated.View
               style={[
