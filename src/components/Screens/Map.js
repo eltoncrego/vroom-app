f2c058d6551740f0192f7193794e74000e6cf8c6
KotovSyndrome <pkatoozian@cma-169-233-9-120.resnet.ucsc.edu>
2018-04-22 00:55:06 -0700

    Installation bug fiiiiiiiinally worked out, Map coming soon^(tm)

---
+++ src/components/Screens/Map.js
@@ -0,0 +1,87 @@
+// /*
+//  * Import all the necessary components for this page.
+//  * Please delete components that aren't used.
+//  */
+
+// // Global Requirements
+// import React, { Component } from 'react';
+// GLOBAL = require('../../Globals');
+// styleguide = require('../../global-styles');
+
+// // Components
+// import {
+//   SafeAreaView,
+//   View,
+//   StyleSheet,
+//   KeyboardAvoidingView,
+//   Platform,
+//   Text,
+//   Alert,
+//   Animated,
+//   TouchableOpacity,
+//   Keyboard,
+// } from 'react-native';
+// import MapView from 'react-native-maps';
+
+// // Our Components
+// import Auth from '../Authentication/Auth';
+// import {InputField} from './../Custom/InputField'
+// import {Button} from './../Custom/Button';
+// import { goTo } from '../Navigation/Navigation';
+// import VroomAlert from './../Custom/VroomAlert';
+
+
+// /*
+//  * Class: Map
+//  * Author: Payam Katoozian
+//  *
+//  * Purpose: Shows a map, for now.
+//  *
+//  */
+// export default class Map extends Component {
+//   /*
+//    * Method: Constructor
+//    * Author: Payam Katoozian
+//    *
+//    * Purpose: Constructs the class with given props
+//    *
+//    * @param: properties
+//    */
+//    constructor(props) {
+//     super(props);
+//    }
+
+//    render() {
+//     return(
+      
+//     )
+//    }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
