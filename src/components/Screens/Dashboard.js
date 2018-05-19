89f3b25ff31b9e9fcf1148c0238c202cd72bb257
Connick Shields <connickshields@gmail.com>
2018-05-19 15:34:28 -0700

    fixed indent :)

--- src/components/Screens/Dashboard.js
+++ src/components/Screens/Dashboard.js
@@ -884,7 +884,7 @@ export default class Dashboard extends Component {
         <View style={styles.content}>
           <Animated.View style={[styles.graph,{opacity: this.state.translation}]}>
               <MPGGraph {...graphProps} />
-        </Animated.View>
+          </Animated.View>
             <Animated.View
               style={[
                 styles.card,
